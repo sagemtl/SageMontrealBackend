ac9df4787c4a44091e9c04e5af37e2ca4d4abb00
Cindy Chen <cxy2015ca@gmail.com>
2021-02-17 23:14:39 -0500

    new inventory seeder format for ktv collection

--- inventorySeeder.js
+++ inventorySeeder.js
@@ -13,119 +13,29 @@ async function populate() {
     const db = client.db(process.env.MONGODB_DBNAME);
     await db.collection('inventory').insertMany([
         {
-            item: 'tshirt',
-            color: 'moss',
-            size: 'S',
-            qty: 7
-        },
-        {
-            item: 'tshirt',
-            color: 'moss',
-            size: 'M',
-            qty: 8
-        },
-        {
-            item: 'tshirt',
-            color: 'moss',
-            size: 'L',
-            qty: 7
-        },
-        {
-            item: 'tshirt',
-            color: 'moss',
-            size: 'XL',
-            qty: 2
-        },
-        {
-            item: 'tshirt',
-            color: 'forest',
-            size: 'S',
-            qty: 8
-        },
-        {
-            item: 'tshirt',
-            color: 'forest',
-            size: 'M',
-            qty: 8
-        },
-        {
-            item: 'tshirt',
-            color: 'forest',
-            size: 'L',
-            qty: 6
-        },
-        {
-            item: 'tshirt',
-            color: 'forest',
-            size: 'XL',
-            qty: 2
-        },
-        {
-            item: 'tshirt',
-            color: 'white',
-            size: 'S',
-            qty: 6
-        },
-        {
-            item: 'tshirt',
-            color: 'white',
-            size: 'M',
-            qty: 4
-        },
-        {
-            item: 'tshirt',
-            color: 'white',
-            size: 'L',
-            qty: 1
-        },
-        {
-            item: 'tshirt',
-            color: 'white',
-            size: 'XL',
-            qty: 2
-        },
-        {
-            item: 'tshirt',
-            color: 'black',
-            size: 'S',
-            qty: 7
-        },
-        {
-            item: 'tshirt',
-            color: 'black',
-            size: 'M',
-            qty: 8
-        },
-        {
-            item: 'tshirt',
-            color: 'black',
-            size: 'L',
-            qty: 4
-        },
-        {
-            item: 'tshirt',
-            color: 'black',
-            size: 'XL',
-            qty: 2
-        },
-        {
             item: 'tote',
-            color: 'canvas',
+            color: 'beige',
             size: 'OS',
-            qty: 18
+            qty: 0
         },
         {
-            item: 'bucket',
-            color: 'dualtone',
+            item: 'hat',
+            color: 'black',
             size: 'OS',
-            qty: 18
+            qty: 0
         },
         {
-            item: 'stickers',
-            color: 'transparent',
-            size: 'OS',
-            qty: 30
-        }
+            item: 'hoodie',
+            color: 'charcoal',
+            size: 'M',
+            qty: 0
+        },
+        {
+            item: 'hoodie',
+            color: 'charcoal',
+            size: 'L',
+            qty: 0
+        },
       ]);
   } finally {
     // Ensures that the client will close when you finish/error
@@ -133,6 +43,123 @@ async function populate() {
   }
 }
 populate().catch(console.dir);
+
+
+// old seeder
+// {
+//     item: 'tshirt',
+//     color: 'moss',
+//     size: 'S',
+//     qty: 7
+// },
+// {
+//     item: 'tshirt',
+//     color: 'moss',
+//     size: 'M',
+//     qty: 8
+// },
+// {
+//     item: 'tshirt',
+//     color: 'moss',
+//     size: 'L',
+//     qty: 7
+// },
+// {
+//     item: 'tshirt',
+//     color: 'moss',
+//     size: 'XL',
+//     qty: 2
+// },
+// {
+//     item: 'tshirt',
+//     color: 'forest',
+//     size: 'S',
+//     qty: 8
+// },
+// {
+//     item: 'tshirt',
+//     color: 'forest',
+//     size: 'M',
+//     qty: 8
+// },
+// {
+//     item: 'tshirt',
+//     color: 'forest',
+//     size: 'L',
+//     qty: 6
+// },
+// {
+//     item: 'tshirt',
+//     color: 'forest',
+//     size: 'XL',
+//     qty: 2
+// },
+// {
+//     item: 'tshirt',
+//     color: 'white',
+//     size: 'S',
+//     qty: 6
+// },
+// {
+//     item: 'tshirt',
+//     color: 'white',
+//     size: 'M',
+//     qty: 4
+// },
+// {
+//     item: 'tshirt',
+//     color: 'white',
+//     size: 'L',
+//     qty: 1
+// },
+// {
+//     item: 'tshirt',
+//     color: 'white',
+//     size: 'XL',
+//     qty: 2
+// },
+// {
+//     item: 'tshirt',
+//     color: 'black',
+//     size: 'S',
+//     qty: 7
+// },
+// {
+//     item: 'tshirt',
+//     color: 'black',
+//     size: 'M',
+//     qty: 8
+// },
+// {
+//     item: 'tshirt',
+//     color: 'black',
+//     size: 'L',
+//     qty: 4
+// },
+// {
+//     item: 'tshirt',
+//     color: 'black',
+//     size: 'XL',
+//     qty: 2
+// },
+// {
+//     item: 'tote',
+//     color: 'canvas',
+//     size: 'OS',
+//     qty: 18
+// },
+// {
+//     item: 'bucket',
+//     color: 'dualtone',
+//     size: 'OS',
+//     qty: 18
+// },
+// {
+//     item: 'stickers',
+//     color: 'transparent',
+//     size: 'OS',
+//     qty: 30
+// }
 
 
 
